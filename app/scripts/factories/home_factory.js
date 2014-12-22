66194a6ce47c7515efc876aec0ca6d42ea9d53be
rdanieldesign <rdanieldesign@gmail.com>
2014-12-21 19:42:06 -0500

    Organize data based on date started

---
+++ app/scripts/factories/home_factory.js
@@ -0,0 +1,21 @@
+(function(){
+
+	angular.module('GAdisasters')
+
+	.factory('HomeFactory', [function(){
+
+		var organizeData = function(data){
+			return _.sortBy(data, function(x){
+				var myDate = x.HAZARD_BEGIN_DATE;
+				return new Date(myDate).getTime();
+			});
+		};
+
+		return {
+			organizeData: organizeData
+		};
+
+
+	}]);
+
+}());
