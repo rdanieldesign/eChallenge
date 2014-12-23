e9572371ab756b1b432320a0a7f35b946b177e17
rdanieldesign <rdanieldesign@gmail.com>
2014-12-22 22:30:55 -0500

    Finish filter functionality

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,13 +8,34 @@
 
 		console.log($scope.disasters);
 
+		$scope.deathsLow = 0;
+		$scope.deathsHigh = 18;
+
 		$scope.deathFilter = function(x){
 			return x.FATALITIES >= $scope.deathsLow && x.FATALITIES <= $scope.deathsHigh;
 		};
 
+		$scope.injuryLow = 0;
+		$scope.injuryHigh = 18;
+
 		$scope.injuryFilter = function(x){
-			return x.INJURIES >= $scope.injuryLow && x.FATALITIES <= $scope.injuryHigh;
+			return x.INJURIES >= $scope.injuryLow && x.INJURIES <= $scope.injuryHigh;
 		};
+
+		$scope.propertyLow = 0;
+		$scope.propertyHigh = 20000000;
+
+		$scope.propertyFilter = function(x){
+			return x.PROPERTY_DAMAGE >= $scope.propertyLow && x.PROPERTY_DAMAGE <= $scope.propertyHigh;
+		};
+
+		$scope.cropLow = 0;
+		$scope.cropHigh = 90000000;
+
+		$scope.cropFilter = function(x){
+			return x.CROP_DAMAGE >= $scope.cropLow && x.CROP_DAMAGE <= $scope.cropHigh;
+		};
+
 
 	}]);
 
