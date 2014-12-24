137348e1d9395299e8766d832edbbb7049ea55e0
rdanieldesign <rdanieldesign@gmail.com>
2014-12-24 11:14:46 -0500

    convert grid to arrows

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -4,34 +4,36 @@
 
 	.controller('Home', ['$scope', 'HomeFactory', function($scope, HomeFactory){
 
-		$scope.disasters = HomeFactory.organizeData(disasters);
+		// $scope.disasters = HomeFactory.organizeData(disasters);
+
+		$scope.disasters2010 = HomeFactory.yearDivide(disasters, 2010);
+
+		$scope.disasters2011 = HomeFactory.yearDivide(disasters, 2011);
+
+		$scope.disasters2012 = HomeFactory.yearDivide(disasters, 2012);
 
 		console.log($scope.disasters);
 
 		$scope.deathsLow = 0;
 		$scope.deathsHigh = 18;
-
 		$scope.deathFilter = function(x){
 			return x.FATALITIES >= $scope.deathsLow && x.FATALITIES <= $scope.deathsHigh;
 		};
 
 		$scope.injuryLow = 0;
 		$scope.injuryHigh = 18;
-
 		$scope.injuryFilter = function(x){
 			return x.INJURIES >= $scope.injuryLow && x.INJURIES <= $scope.injuryHigh;
 		};
 
 		$scope.propertyLow = 0;
 		$scope.propertyHigh = 20000000;
-
 		$scope.propertyFilter = function(x){
 			return x.PROPERTY_DAMAGE >= $scope.propertyLow && x.PROPERTY_DAMAGE <= $scope.propertyHigh;
 		};
 
 		$scope.cropLow = 0;
 		$scope.cropHigh = 90000000;
-
 		$scope.cropFilter = function(x){
 			return x.CROP_DAMAGE >= $scope.cropLow && x.CROP_DAMAGE <= $scope.cropHigh;
 		};
--- app/scripts/factories/home_factory.js
+++ app/scripts/factories/home_factory.js
@@ -11,8 +11,21 @@
 			});
 		};
 
+		var yearDivide = function(data, year){
+			var filtered =  _.filter(data, function(x){
+				var beginDate = x.HAZARD_BEGIN_DATE;
+				var date = new Date(beginDate).getFullYear();
+				return year == date;
+			});
+			return _.sortBy(filtered, function(x){
+				var myDate = x.HAZARD_BEGIN_DATE;
+				return new Date(myDate).getTime();
+			});
+		};
+
 		return {
-			organizeData: organizeData
+			organizeData: organizeData,
+			yearDivide: yearDivide
 		};
 
 	}]);
